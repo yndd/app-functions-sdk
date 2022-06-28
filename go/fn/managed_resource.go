bef0e7f967bc659f639295a7ec9b1c5da8027946
Wim Henderickx <wim.henderickx@gmail.com>
2022-06-28 12:37:13 +0200

    added kubeObject iso managed.resource

--- go/fn/managed_resource.go
+++ go/fn/managed_resource.go
@@ -2,8 +2,6 @@ package fn
 
 import (
 	"fmt"
-
-	"sigs.k8s.io/kustomize/kyaml/kio"
 )
 
 // ParseManagedResource parses a Managed Resource from the input byte array. This function can be used to parse either KRM fn input
@@ -15,49 +13,49 @@ func ParseManagedResource(in []byte) (*KubeObject, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse input bytes: %w", err)
 	}
-	if mrObj.GetKind() != kio.ResourceListKind {
-		return nil, fmt.Errorf("input was of unexpected kind %q; expected ResourceList", mrObj.GetKind())
-	}
 	/*
-		// Parse FunctionConfig. FunctionConfig can be empty, e.g. `kubeval` fn does not require a FunctionConfig.
-		fc, found, err := mrObj.obj.GetNestedMap("functionConfig")
-		if err != nil {
-			return nil, fmt.Errorf("failed when tried to get functionConfig: %w", err)
+		if mrObj.GetKind() != kio.ResourceListKind {
+			return nil, fmt.Errorf("input was of unexpected kind %q; expected ResourceList", mrObj.GetKind())
 		}
-		if found {
-			rl.FunctionConfig = asKubeObject(fc)
-		} else {
-			rl.FunctionConfig = NewEmptyKubeObject()
-		}
-
-		// Parse Items. Items can be empty, e.g. an input ResourceList for a generator function may not have items.
-		items, found, err := rlObj.obj.GetNestedSlice("items")
-		if err != nil {
-			return nil, fmt.Errorf("failed when tried to get items: %w", err)
-		}
-		if found {
-			objectItems, err := items.Elements()
+			// Parse FunctionConfig. FunctionConfig can be empty, e.g. `kubeval` fn does not require a FunctionConfig.
+			fc, found, err := mrObj.obj.GetNestedMap("functionConfig")
 			if err != nil {
-				return nil, fmt.Errorf("failed to extract objects from items: %w", err)
+				return nil, fmt.Errorf("failed when tried to get functionConfig: %w", err)
 			}
-			for i := range objectItems {
-				rl.Items = append(rl.Items, asKubeObject(objectItems[i]))
+			if found {
+				rl.FunctionConfig = asKubeObject(fc)
+			} else {
+				rl.FunctionConfig = NewEmptyKubeObject()
 			}
-		}
 
-		// Parse Results. Results can be empty.
-		res, found, err := rlObj.obj.GetNestedSlice("results")
-		if err != nil {
-			return nil, fmt.Errorf("failed when tried to get results: %w", err)
-		}
-		if found {
-			var results Results
-			err = res.Node().Decode(&results)
+			// Parse Items. Items can be empty, e.g. an input ResourceList for a generator function may not have items.
+			items, found, err := rlObj.obj.GetNestedSlice("items")
 			if err != nil {
-				return nil, fmt.Errorf("failed to decode results: %w", err)
+				return nil, fmt.Errorf("failed when tried to get items: %w", err)
 			}
-			rl.Results = results
-		}
+			if found {
+				objectItems, err := items.Elements()
+				if err != nil {
+					return nil, fmt.Errorf("failed to extract objects from items: %w", err)
+				}
+				for i := range objectItems {
+					rl.Items = append(rl.Items, asKubeObject(objectItems[i]))
+				}
+			}
+
+			// Parse Results. Results can be empty.
+			res, found, err := rlObj.obj.GetNestedSlice("results")
+			if err != nil {
+				return nil, fmt.Errorf("failed when tried to get results: %w", err)
+			}
+			if found {
+				var results Results
+				err = res.Node().Decode(&results)
+				if err != nil {
+					return nil, fmt.Errorf("failed to decode results: %w", err)
+				}
+				rl.Results = results
+			}
 	*/
 	return mrObj, nil
 }
